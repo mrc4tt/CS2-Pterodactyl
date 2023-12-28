00b51272de1ea55b827d0be8716b65abf3dccd17
Mikkel / Miksen <mikkel.linddk@gmail.com>
2023-12-28 14:53:06 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,6 @@ export INTERNAL_IP
 
 # old METAMOD_LATEST="https://sourcemm.net/latest.php?os=linux&version=2.0"
 METAMOD_LATEST="https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git1227-linux.tar.gz"
-GAMEINFO_LATEST="https://mrc4t.xyz/cs2fix.tar.gz"
 
 print() {
     echo -e "$1"
@@ -40,12 +39,6 @@ download_default_stable() {
     curl --location --output metamod.tar.gz "$METAMOD_LATEST"
 }
 
-download_default_patch() {
-    print_bold_white "Defaulting to and downloading the latest GameInfoPatch"
-    curl --location --output cs2fix.tar.gz "$GAMEINFO_LATEST"
-}
-
-#
 INSTALL_PATH="${INSTALL_PATH:-game/csgo/}"
 
 detect_install_path() {
@@ -87,34 +80,6 @@ if [[ "${METAMOD}" = 1 || "${METAMOD}" == "true" ]]; then
     tar -xf metamod.tar.gz --directory /home/container/"${INSTALL_PATH}"
     rm -rf "/home/container/${INSTALL_PATH}/tmpfiles"
     print_green "Metamod has been installed!\n"
-fi
-
-    # Just some random shit.
-if [[ "${GAMEINFO}" = 1 || "${GAMEINFO}" == "true" ]]; then
-    GAMEINFO_LATEST="https://mrc4t.xyz/cs2fix.tar.gz"
-    cd /home/container || exit 1    
-    
-    print_yellow "Installing GameInfo PATCH..."
-    # Should custom versions be provided, check that they are valid. If not, use the latest stable version.
-    if [[ -n "${GAMEINFO_VERSION}" ]]; then
-        GAMEINFO_LATEST="https://mrc4t.xyz/cs2fix.tar.gz"
-    fi
-    
-    if [[ -z ${GAMEINFO_LATEST} ]]; then
-        download_default_patch
-    else
-        if is_valid_url "${GAMEINFO_LATEST}"; then
-                curl -L -O "${GAMEINFO_LATEST}" > /home/container/cs2fix.tar.gz
-            else          
-                download_default_patch
-            fi
-    fi
-
-    # Extract GamePatch
-    print_bold_white "Extracting GAMEINFO PATCH"
-    tar -xf cs2fix.tar.gz --directory /home/container/
-    rm "/home/container/cs2fix.tar.gz"
-    print_green "GAMEINFO patch has been installed!\n"
 fi
     
 # Update Source Server
@@ -172,8 +137,31 @@ if [ ! -z ${SRCDS_APPID} ]; then
     fi
 fi
 
-cd /home/container || exit 1
+# Edit /home/container/game/csgo/gameinfo.gi to add MetaMod path
+# Credit: https://github.com/ghostcap-gaming/ACMRS-cs2-metamod-update-fix/blob/main/acmrs.sh
+GAMEINFO_FILE="/home/container/game/csgo/gameinfo.gi"
+GAMEINFO_ENTRY="            Game    csgo/addons/metamod"
+if [ -f "${GAMEINFO_FILE}" ]; then
+    if grep -Fxq "$GAMEINFO_ENTRY" "$GAMEINFO_FILE"; then
+        echo "File gameinfo.gi already configured. No changes were made."
+    else
+        awk -v new_entry="$GAMEINFO_ENTRY" '
+            BEGIN { found=0; }
+            // {
+                if (found) {
+                    print new_entry;
+                    found=0;
+                }
+                print;
+            }
+            /Game_LowViolence/ { found=1; }
+        ' "$GAMEINFO_FILE" > "$GAMEINFO_FILE.tmp" && mv "$GAMEINFO_FILE.tmp" "$GAMEINFO_FILE"
 
+        echo "The file ${GAMEINFO_FILE} has been configured successfully."
+    fi
+fi
+
+# cd /home/container || exit 1
 # Replace Startup Variables
 # shellcheck disable=SC2086
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
